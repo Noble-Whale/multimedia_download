ba9dc1cf90383f83d3c34dc90650970e4fe1863b
HJ <asurava@naver.com>
2025-05-14 21:29:56 +0900

    Upload

--- multimedia_down.py
+++ multimedia_down.py
@@ -3,8 +3,7 @@ import re
 import traceback
 
 ##-------------------------------youtube, movie start
-import pytubefix # youtube에서 로직 및 파라미터를 계속 패치하는 것 같은데, 안되면 pip install pytubefix --upgrade로 최신버전 받아볼 것
-from moviepy import *
+import yt_dlp
 ##-------------------------------youtube, movie end
 
 ##-------------------------------image start
@@ -17,22 +16,21 @@ options.add_argument('headless')
 ##-------------------------------image end
 
 def movie_down(URL, target='movie'):
-    yt = pytubefix.YouTube
-
-    video_content = yt(URL, "WEB") # nodejs 필요, python interpreter 3.13 ↑
-
-    print(video_content.title)
-
-    video = video_content.streams.get_highest_resolution()
-
-    yt_filename = re.sub("(/|\.mp4)", "", video.default_filename) # ①파일 이름에 /가 들어가면 경로로 인식해서 moviepy가 오류를 떨궈서 문자 대체
-                                                                            # ②확장자(.mp4)를 빼서 순수한 파일명만 추출
-
-    os.rename(video.download(), yt_filename + ".mp4") # 다운로드 하고, 완료된 파일의 파일명도 변경함
-
     if target == "music":
-        mvpy_video = VideoFileClip(yt_filename + ".mp4")
-        mvpy_video.audio.write_audiofile(yt_filename + ".mp3")
+        ydl_opts = {
+            'format': 'bestaudio',
+            'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}]
+        }
+    else:
+        ydl_opts = {
+            'outtmpl': '%(title)s.%(ext)s',  # 저장할 파일 이름 형식
+            'format': 'bestvideo+bestaudio/best',  # 최고 화질/음질 조합
+            'merge_output_format': 'mp4',  # 비디오와 오디오 병합 포맷
+            'writesubtitles': True, 'subtitleslangs': ['ko'] # 한글 자막 있으면 자막 포함
+        }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([URL])
 
 
 def image_down(URL):
